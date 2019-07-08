1c4e1d46f7f204c860330aa52baaa45f931090f1
79069 <nastactizh23@gmail.com>
2019-07-08 16:32:11 +0700

    hometask_2 additional tasks

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -10,7 +10,7 @@ fun User.toUserView():UserView{
 
     val nickName=Utils.transliteration("$firstName $lastName")
     val initials=Utils.toInitials(firstName,lastName)
-    val status=if(lastName==null)"Еще ни разу не был" else if(isOnline)"online" else "Последний раз был ${lastVisit.humanizeDiff()}"
+    val status=if(lastName==null)"Еще ни разу не был" else if(isOnline)"online" else "Последний раз был ${lastVisit?.humanizeDiff()}"
 
     return UserView(
         id,
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -15,11 +15,177 @@ object Utils {
 
     }
 
-    fun transliteration(payLoad: String,divider:String=" "): String {
-        TODO("not implemented")
+    fun transliteration(payLoad: String, divider: String = " "): String {
+        val mapping = payLoad.map {
+
+            when (it) {
+
+                ' ' -> divider
+
+                'а' -> 'a'
+
+                'б' -> 'b'
+
+                'в' -> 'v'
+
+                'г' -> 'g'
+
+                'д' -> 'd'
+
+                'е' -> 'e'
+
+                'ё' -> 'e'
+
+                'ж' -> "zh"
+
+                'з' -> 'z'
+
+                'и' -> 'i'
+
+                'й' -> 'i'
+
+                'к' -> 'k'
+
+                'л' -> 'l'
+
+                'м' -> 'm'
+
+                'н' -> 'n'
+
+                'о' -> 'o'
+
+                'п' -> 'p'
+
+                'р' -> 'r'
+
+                'с' -> 's'
+
+                'т' -> 't'
+
+                'у' -> 'u'
+
+                'ф' -> 'f'
+
+                'х' -> 'h'
+
+                'ц' -> 'c'
+
+                'ч' -> "ch"
+
+                'ш' -> "sh"
+
+                'щ' -> "sh'"
+
+                'ъ' -> ""
+
+                'ы' -> 'i'
+
+                'ь' -> ""
+
+                'э' -> 'e'
+
+                'ю' -> "yu"
+
+                'я' -> "ya"
+
+                'А' -> 'A'
+
+                'Б' -> 'B'
+
+                'В' -> 'V'
+
+                'Г' -> 'G'
+
+                'Д' -> 'D'
+
+                'Е' -> 'E'
+
+                'Ё' -> 'E'
+
+                'Ж' -> "Zh"
+
+                'З' -> 'Z'
+
+                'И' -> 'I'
+
+                'Й' -> 'I'
+
+                'К' -> 'K'
+
+                'Л' -> 'L'
+
+                'М' -> 'M'
+
+                'Н' -> 'N'
+
+                'О' -> 'O'
+
+                'П' -> 'P'
+
+                'Р' -> 'R'
+
+                'С' -> 'S'
+
+                'Т' -> 'T'
+
+                'У' -> 'U'
+
+                'Ф' -> 'F'
+
+                'Х' -> 'H'
+
+                'Ц' -> 'C'
+
+                'Ч' -> "Ch"
+
+                'Ш' -> "Sh"
+
+                'Щ' -> "Sh'"
+
+                'Ъ' -> ""
+
+                'Ы' -> 'I'
+
+                'Ь' -> ""
+
+                'Э' -> 'E'
+
+                'Ю' -> "Yu"
+
+                'Я' -> "Ya"
+
+                else -> it
+
+            }
+
+        }
+        return mapping.joinToString(separator = "")
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("not implemented")
+        var strf = firstName
+        var strl = lastName
+        return (if (firstName.isNullOrEmpty() && lastName.isNullOrEmpty()) null
+        else if (firstName.isNullOrEmpty()) strl!![0].toString()
+        else if (lastName.isNullOrEmpty()) strf!![0].toString()
+        else
+            strf!![0].toString() + strl!![0].toString())
+
+    }
+    fun getPluralForm(pluralForms: String, count: Int): String {
+
+        val forms = pluralForms.split(";")
+
+        when (count % 10) {
+
+            1 -> if (count % 100 != 11)
+                return forms[0]
+
+            in 2..4 -> if (count % 100 !in 12..14) {
+                return forms[1]
+            }
+        }
+        return forms[2]
+
     }
 }
